a96f69e31b254b8cf8d0af0e8731680297bbf685
moustafa <mostafa.mahmoud99@hotmail.com>
2022-01-06 12:33:32 +0300

    id

--- myapi/serializers.py
+++ myapi/serializers.py
@@ -5,4 +5,4 @@ from .models import sensor
 class sensorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = sensor
-        fields = ('temp', 'ic', 'dist')
+        fields = ('id', 'temp', 'ic', 'dist')
