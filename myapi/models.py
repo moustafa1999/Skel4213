77a57a9f32c08906e3e23a7b04fc33b859ca6ead
moustafa <mostafa.mahmoud99@hotmail.com>
2022-02-21 17:09:23 +0300

    alterations

--- myapi/models.py
+++ myapi/models.py
@@ -5,7 +5,7 @@ class sensor(models.Model):
     temp = models.CharField(max_length=60)
     ic = models.CharField(max_length=60)
     dist =  models.CharField(max_length=60)
-    time = models.TimeField()
+    time =  models.CharField(max_length=60)
     def __str__(self):
         return self.temp
         return self.id
