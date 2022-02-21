8085b44bb84058053c42d585f861d860a7e21443
moustafa <mostafa.mahmoud99@hotmail.com>
2022-02-21 16:48:03 +0300

    time model

--- myapi/models.py
+++ myapi/models.py
@@ -5,7 +5,9 @@ class sensor(models.Model):
     temp = models.CharField(max_length=60)
     ic = models.CharField(max_length=60)
     dist =  models.CharField(max_length=60)
+    time = models.TimeField()
     def __str__(self):
         return self.temp
         return self.id
         return self.dist
+        return self.time
